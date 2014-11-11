7adbb4852ba0815d5458cc5b7399c3ff80784cc6
dmontenegro <dmontenegro92@gmail.com>
2014-11-11 15:35:18 -0500

    Proyect.

---
+++ inventories/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Inventory(models.Model):
+    resume = models.CharField(max_length=200)
+    date = models.DateTimeField('date')
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.resume
